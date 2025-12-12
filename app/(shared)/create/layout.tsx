0b3c25756c043ea9c5a94f54b640c8f96d8325d4
Ifeoluwah <spencerife.21@gmail.com>
2025-12-12 17:38:15 +0100

    feat: build create page
    - ui skeleton for create page, no logic yet
    - make the page responsive

---
+++ app/(shared)/create/layout.tsx
@@ -0,0 +1,22 @@
+import type { FC, PropsWithChildren } from "react";
+
+const CreateLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <main className="mx-auto w-full max-w-[1600px] flex-1 px-4 py-5 sm:px-8 md:px-16 lg:px-24 xl:px-40">
+      <div className="flex w-full max-w-7xl flex-col p-4">
+        <div className="mb-8 flex flex-col gap-3">
+          <h1 className="text-4xl leading-normal font-black tracking-[-0.033rem] text-white">
+            Create New NFT
+          </h1>
+          <p className="text-custom-text text-base leading-normal font-medium">
+            Upload your digital asset and provide necessary details to mint it
+            as NFT.
+          </p>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default CreateLayout;
---
+++ app/(shared)/create/loading.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const loading: FC = () => {
+  return <div>loading create page...</div>;
+};
+
+export default loading;
